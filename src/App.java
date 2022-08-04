b0ebacbee277e4b97305a88b3f98261c0bc8a820
LuanEvander <luanevander.dev@gmail.com>
2022-08-04 18:58:29 -0300

    Criação de classes e método de leitura de arquivo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,25 @@
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("Olá gente!");
+        csvReader();
+    }
+
+    static void csvReader() {
+        String path = "B:/aulaAlgoritmos/trabalhoGrupoAlgoritmos2/assets/Entrevista.csv";
+        String line = "";
         
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+                while ((line = reader.readLine()) != null) {
+                    /*String[] row = line.split("; "); // método split para separar colunas
+                    System.out.println(row[4]);*/ //sysout para selecionar a coluna
+                    System.out.println(line);
+                }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Entrevistado.java
+++ src/Entrevistado.java
@@ -1,3 +1,14 @@
-public class Entrevistado {
-    
+import java.io.*;
+
+public class Entrevistado implements Serializable {
+    private String sexo;
+    private String idade;
+    private String escolaridade;
+    private String moradia;
+    private String tecnologia;
+    private String prioridade;
+
+    public String toString () {
+        return this.sexo + " ; " + this.idade + " ; " + this.escolaridade + " ; " + this.moradia + " ; " + this.tecnologia + " ; " + this.prioridade;
+    }
 }
