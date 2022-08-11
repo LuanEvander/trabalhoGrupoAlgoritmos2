6e4d31e22a8d6bdc5ed7aeb971619337c3fc9f8b
KimiMitsuji <KennyMHT7@hotmail.com>
2022-08-11 15:58:49 -0300

    javadoc
    a criação do JavaDoc e comentários

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 
+//A classe Main serve para ler o arquivo de entrada e cria um arquivo txt com o resultado.
 public class Main {
     public static void main(String[] args) throws Exception {
         EntrevistadoVetor callMethod = csvReader();
@@ -7,11 +8,19 @@ public class Main {
         System.out.println("Relatório gerado com sucesso!");
     }
 
+
+    /**
+     * @return EntrevistadoVetor
+     * O método csvReader() lê o arquivo de entrada e cria um vetor de objetos Entrevistado.
+     * @author Kauã Henrico
+     */
+
     static EntrevistadoVetor csvReader() {
         String path = "Entrevista.csv";
         String line = "";
         EntrevistadoVetor entrevistadoVetor = new EntrevistadoVetor();
 
+        // esse try é para ler o arquivo csv e quais as informações que serão lidas e até onde vão ser lidas.
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path));
             while ((line = reader.readLine()) != null) {
@@ -19,12 +28,15 @@ public class Main {
                 Entrevistado entrevistado = new Entrevistado(row[0].charAt(0), row[1], row[2], row[3], row[4], row[5]);
                 entrevistadoVetor.add(entrevistado);
             }
+
+            // esse catch é para caso o arquivo não seja encontrado
             reader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // retorna o vetor de objetos Entrevistado
         return entrevistadoVetor;
     }
 }
